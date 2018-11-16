afd11fe902d007acb4de525a74da2a7b11702a08
Peng Lyu <penn.lv@gmail.com>
2018-11-16 15:44:19 -0800

    More word separators.

--- src/extension.ts
+++ src/extension.ts
@@ -23,7 +23,7 @@ const runHunspell = (hunspellFactory, affPath, dicPath) => {
 
 async function init(context) {
 	const hunspellFactory = await hunspellASM.loadModule();
-	const mountedPath = hunspellFactory.mountDirectory(path.resolve('/Users/penlv/Documents/dictionaries'));
+	const mountedPath = hunspellFactory.mountDirectory(path.resolve('C:\\Users\\penlv\\Documents\\Dictionary'));
 	const dictFile = unixify(path.join(mountedPath, 'English.dic'));
 	const affFile = unixify(path.join(mountedPath, 'English.aff'));
 	const hunspell = hunspellFactory.create(affFile, dictFile);
@@ -40,23 +40,27 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	let hunspell = await init(context);
 	context.subscriptions.push(hunspell);
-    let disposable = vscode.commands.registerCommand('extension.spell', async () => {
-        // The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		let editor = vscode.window.activeTextEditor;
+	const collection = vscode.languages.createDiagnosticCollection('spell');
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(async e => {
+		let editor = e;
 		if (!editor) {
 			return;
 		}
 
-		const collection = vscode.languages.createDiagnosticCollection('spell');
 		const misSpells: vscode.Diagnostic[] = [];
 
 		var start: any = new Date()
 		let document = editor.document;
-		let tokenizer = await monarch.getLanguageSupport('typescript');
+		let tokenizer;
+		try {
+			tokenizer = await monarch.getLanguageSupport(document.languageId);
+		} catch (e) {}
+		if(!tokenizer) {
+			return;
+		}
 		let state = tokenizer.getInitialState();
 		let isInSideComment = false;
-		let wordSeparator = new RegExp(/[.=+{}\s*()\[,@#&!/<>:\-\'\"]+/, 'g');
+		let wordSeparator = new RegExp(/[.=+{}\s*()\[\]_`\\|,@#&!/<>:\-\'\"]+/, 'g');
 		for (let i = 0; i < document.lineCount; i++) {
 			let line = document.lineAt(i);
 			let ret = tokenizer.tokenize(line.text, state, 0);
@@ -108,42 +112,19 @@ export async function activate(context: vscode.ExtensionContext) {
 								range: new vscode.Range(i, startIndex + lastCharacter, i, startIndex + matchStartIndex),
 								severity: vscode.DiagnosticSeverity.Warning
 							})
-							console.log(word);
+							// console.log(word);
 						}
 					}
 					lastCharacter = matchStartIndex + matchLength;
 				} while(m);
-
-				// let previousNonWordIndex = 0;
-				// while(matches) {
-				// 	let index =
-				// 	matches = wordSeparator.exec(text);
-				// }
-				// if (matches && matches.length) {
-				// 	name = text.substring(haracter, preview.match.end.character + matches.index);
-				// }
-
 			}
 			state = ret.endState;
-
-			// for (let j = 0; j < words.length; j++) {
-			// 	let word = words[j];
-			// 	let misSpell = hunspell.spell(word);
-			// 	if (!misSpell) {
-			// 		console.log(word);
-			// 	}
-			// }
 		}
 
 		var end = <any>(new Date()) - start
 		console.info('Execution time: %dms', end)
 		collection.set(document.uri, misSpells);
-		// hunspell.addWord('github');
-		// let misSpell = hunspell.spell('github');
-		// vscode.window.showInformationMessage(`check spell for word 'github': ${misSpell}`);
-    });
-
-    context.subscriptions.push(disposable);
+	}));
 }
 
 // this method is called when your extension is deactivated
